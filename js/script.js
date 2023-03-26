fcdd0d18476298bc3ce559d835c095b4230a563a
Mauricio Maciel <127659564+MauricioOMaciel@users.noreply.github.com>
2023-03-26 19:08:29 -0300

    Portfolio finalmente pronto
    Desenvolvi sozinho usando bootstrap, Javascript, HTML e CSS.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var botao = document.getElementById('mostrar-area');
+var area = document.getElementById('area-oculta');
+
+botao.addEventListener('click', function() {
+   area.style.display = 'block';
+});
